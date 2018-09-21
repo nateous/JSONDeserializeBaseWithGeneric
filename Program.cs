c5ffa497c77c97c1409beff66d02fe45ea31c1b6
nateous <natebell.com@gmail.com>
2018-09-21 17:10:19 -0400

    WIP part 2

--- Program.cs
+++ Program.cs
@@ -8,22 +8,31 @@ namespace JSONDeserializeBaseWithGeneric
     {
         static void Main(string[] args)
         {
-            var response1Get = "{\"value\":{\"Name\":\"Bob\"}}";
-            var response1List = "{\"value\":[{\"Name\":\"Bob\"},{\"Name\":\"Jennifer\"},{\"Name\":\"Mary\"}]}";
-            var response2Get = "{\"value\":{\"Super\":\"Bob\"}}";
-            var response2List = "{\"value\":[{\"Super\":\"Bob\"},{\"Super\":\"Jennifer\"},{\"Super\":\"Mary\"}]}";
-
-            var get1 = JsonConvert.DeserializeObject<BaseResponse<NameObj>>(response1Get);
+            var nameObj = new NameObj();
+            var get1 = JsonConvert.DeserializeObject<BaseResponse<NameObj>>(nameObj.GetBaseJSON);
             Console.WriteLine($"get1 is null: {get1 == null}");
             Console.WriteLine($"get1.value: {get1.value}");
             var name1 = get1.Get();
             Console.WriteLine($"name1 is null: {name1 == null}");
 
-            var getList1 = JsonConvert.DeserializeObject<BaseResponse<NameObj>>(response1List);
+            var getList1 = JsonConvert.DeserializeObject<BaseResponse<NameObj>>(nameObj.ListBaseJSON);
             Console.WriteLine($"getList1 is null: {getList1 == null}");
             Console.WriteLine($"getList1.value: {getList1.value}");
             var nameList = getList1.List();
             Console.WriteLine($"nameList is null: {nameList == null}");
+
+            var superObj = new SuperObj();
+            var get2 = JsonConvert.DeserializeObject<BaseResponse<SuperObj>>(superObj.GetBaseJSON);
+            Console.WriteLine($"get2 is null: {get2 == null}");
+            Console.WriteLine($"get2.value: {get2.value}");
+            var name2 = get2.Get();
+            Console.WriteLine($"name2 is null: {name2 == null}");
+
+            var getList2 = JsonConvert.DeserializeObject<BaseResponse<SuperObj>>(superObj.ListBaseJSON);
+            Console.WriteLine($"getList2 is null: {getList2 == null}");
+            Console.WriteLine($"getList2.value: {getList2.value}");
+            var nameList2 = getList2.List();
+            Console.WriteLine($"nameList is null: {nameList2 == null}");
 
             Console.WriteLine("Hello World!");
         }
@@ -31,16 +40,27 @@ namespace JSONDeserializeBaseWithGeneric
 
     public class BaseObj
     {
-        public string SomeBaseProperty { get; set; }
+        public string GetBaseJSON { get; set; } = string.Empty;
+        public string ListBaseJSON { get; set; } = string.Empty;
     }
 
     public class NameObj : BaseObj
     {
+        public NameObj()
+        {
+            GetBaseJSON = "{\"value\":{\"Name\":\"Bob\"}}";
+            ListBaseJSON = "{\"value\":[{\"Name\":\"Bob\"},{\"Name\":\"Jennifer\"},{\"Name\":\"Mary\"}]}";
+        }
         public string Name { get; set; }
     }
 
     public class SuperObj : BaseObj
     {
+        public SuperObj()
+        {
+            GetBaseJSON = "{\"value\":{\"Super\":\"Bob\"}}";
+            ListBaseJSON = "{\"value\":[{\"Super\":\"Bob\"},{\"Super\":\"Jennifer\"},{\"Super\":\"Mary\"}]}";
+        }
         public string Super { get; set; }
     }
 
